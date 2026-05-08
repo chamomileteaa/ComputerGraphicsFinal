f3459fff23ac4b7336cf00079c22c1a0c4a06bb5
Camille Treichler <camilletreichler@icloud.com>
2026-05-08 22:58:23 +1000

    Base

---
+++ src/main.js
@@ -0,0 +1,84 @@
+import * as THREE from '../build/three.module.js';
+import { OrbitControls } from '../build/controls/OrbitControls.js';
+
+import { createScene, createCamera, createRenderer } from './setup.js';
+
+const canvas = document.querySelector('#app');
+
+const scene = createScene();
+const camera = createCamera();
+const renderer = createRenderer(canvas);
+
+const grid = new THREE.GridHelper(50, 50);
+scene.add(grid);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// smooth movement
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
+controls.minDistance = 2;
+controls.maxDistance = 50;
+
+
+//
+// FLOOR
+//
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(50, 50),
+    new THREE.MeshStandardMaterial({ color: 0x808080 })
+);
+
+floor.rotation.x = -Math.PI / 2;
+floor.receiveShadow = true;
+
+scene.add(floor);
+
+//
+// LIGHTING
+//
+const ambient = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(ambient);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+dirLight.position.set(5, 10, 5);
+dirLight.castShadow = true;
+
+scene.add(dirLight);
+
+//
+// TEST OBJECT
+//
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(),
+    new THREE.MeshStandardMaterial({ color: 0x00ffcc })
+);
+
+cube.position.y = 5.5;
+cube.castShadow = true;
+
+scene.add(cube);
+
+//
+// LOOP
+//
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+animate();
+
+//
+// RESIZE HANDLING
+//
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
---
+++ src/setup.js
@@ -0,0 +1,29 @@
+import * as THREE from '../build/three.module.js';
+
+export function createScene() {
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x202025);
+    return scene;
+}
+
+export function createCamera() {
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+
+    camera.position.set(8, 6, 8);
+    camera.lookAt(0, 0, 0);
+    return camera;
+}
+
+export function createRenderer(canvas) {
+    const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+
+    return renderer;
+}
